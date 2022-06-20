3d2d0425beabdf2a3fec44d0dd443e9233ff0a79
cheburakshu <rajagopalasu@vmware.com>
2022-06-20 10:14:07 -0600

    black

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ['SSH_DIR'] = '/root/.ssh'
-os.environ['KAGGLE_DIR'] = '/root/.kaggle'
-os.environ['RUN_DIR'] = '/opt'
-os.environ['DATA_DIR'] = '/kaggle/working/data'
+os.environ["SSH_DIR"] = "/root/.ssh"
+os.environ["KAGGLE_DIR"] = "/root/.kaggle"
+os.environ["RUN_DIR"] = "/opt"
+os.environ["DATA_DIR"] = "/kaggle/working/data"
